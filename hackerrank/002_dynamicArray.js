3c3678dea3ac7bd7c82d30607b925717228610c3
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-08-04 13:41:48 -0700

    complete

---
+++ hackerrank/002_dynamicArray.js
@@ -0,0 +1,21 @@
+function dynamicArray(n, queries) {
+  let answer = [];
+  let arr = [];
+  let lastAnswer = 0;
+  for (let i = 0; i < n; i++) {
+      arr.push([]);
+  }
+  for (const query of queries) {
+      let num = query[0];
+      let x = query[1];
+      let y = query[2];
+      let idx = ((x ^ lastAnswer) % n);
+      if (num === 1) {
+          arr[idx].push(y);
+      } else if (num === 2) {
+          lastAnswer = arr[idx][y % arr[idx].length];
+          answer.push(lastAnswer);
+      }
+  }
+  return answer;
+}
