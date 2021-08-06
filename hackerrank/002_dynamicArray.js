013d7e4c656ffa9aa325ecf1a6abd3f4dcb87c47
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-08-05 19:00:58 -0700

    fix tabbing

--- hackerrank/002_dynamicArray.js
+++ hackerrank/002_dynamicArray.js
@@ -3,19 +3,19 @@ function dynamicArray(n, queries) {
   let arr = [];
   let lastAnswer = 0;
   for (let i = 0; i < n; i++) {
-      arr.push([]);
+    arr.push([]);
   }
   for (const query of queries) {
-      let num = query[0];
-      let x = query[1];
-      let y = query[2];
-      let idx = ((x ^ lastAnswer) % n);
-      if (num === 1) {
-          arr[idx].push(y);
-      } else if (num === 2) {
-          lastAnswer = arr[idx][y % arr[idx].length];
-          answer.push(lastAnswer);
-      }
+    let num = query[0];
+    let x = query[1];
+    let y = query[2];
+    let idx = ((x ^ lastAnswer) % n);
+    if (num === 1) {
+        arr[idx].push(y);
+    } else if (num === 2) {
+    lastAnswer = arr[idx][y % arr[idx].length];
+    answer.push(lastAnswer);
+    }
   }
   return answer;
 }
