bc7ae8d656b2374b68f9fd1bc7aafee4fc354765
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-08-20 23:51:43 -0700

    complete

--- leetcode/038_findTheDuplicateNumber.js
+++ leetcode/038_findTheDuplicateNumber.js
@@ -15,9 +15,30 @@
  */
 
 const findDuplicate = function(nums) {
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) return nums[i];
+  // nums.sort((a, b) => a - b);
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] === nums[i + 1]) return nums[i];
+  // }
+  // return 0;
+  
+  let high = nums.length - 1;
+  let low = 0;
+  let dup = -1;
+  
+  while (high >= low) {
+    let count = 0;
+    let mid = low + Math.floor((high - low)/2);
+    for (const num of nums) {
+      if (num <= mid) {
+        count++;
+      }
+    }
+    if (count > mid) {
+      dup = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
   }
-  return 0;
+  return dup;
 };
